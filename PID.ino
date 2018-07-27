28ede90a5b0d5c1af3a9c23d17c6045c0f0cd599
Andreas Ortner <office@gh-ortner.com>
2018-07-27 13:20:14 +0200

    Add files via upload

---
+++ PID.ino
@@ -0,0 +1,26 @@
+
+void calcSteeringPID(void) {
+  
+if (distanceFromLine <50 && distanceFromLine >-50)
+{ //Proportional
+  pValue = steerAngleSetPoint*steerSensorCounts+steeringPositionZero;
+  pValue = pValue + distanceFromLine*Ko*0.1;
+  }
+else
+{
+   //Proportional
+  pValue = steerAngleSetPoint*steerSensorCounts+steeringPositionZero;
+  } 
+ }
+
+ void motorDrive(void) 
+  {
+    jrk.setTarget(pValue);
+  }
+
+void updatejrk(){
+  jrk.setProportionalCoefficient(Kp, 1);
+  jrk.setDerivativeCoefficient(Kd, 5);
+  jrk.setIntegralCoefficient(Ki, 1);
+    
+  }
